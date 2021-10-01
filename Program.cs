f304665a2d3f37bf5fbc57dbfccb1607a21e9bca
TwinB0rn <twinb0rn@protonmail.com>
2021-10-01 14:26:29 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int num =0; num <=100; num++)
+            {
+                if ((num % 3 == 0) && (num % 5 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
